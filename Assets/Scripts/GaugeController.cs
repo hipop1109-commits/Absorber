f3c9e29544a1532e9a3e5a0bc235e22249a73242
kydyne <dainkk55@gmail.com>
2025-01-14 15:37:45 +0900

    UI Controller
    UI

---
+++ Assets/Scripts/GaugeController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeController : MonoBehaviour
+{
+    // 게이지 이미지
+    public Image rockGaugeImage; 
+    public Image grassGaugeImage;
+    public Image waterGaugeImage;
+
+    private void Update()
+    {
+        // 게이지 차오르는 이미지
+        rockGaugeImage.fillAmount = WeaponController.Instance.RockGauge / WeaponController.Instance.MaxGauge;
+        grassGaugeImage.fillAmount = WeaponController.Instance.GrassGauge / WeaponController.Instance.MaxGauge;
+        waterGaugeImage.fillAmount = WeaponController.Instance.WaterGauge / WeaponController.Instance.MaxGauge;
+    }
+}
