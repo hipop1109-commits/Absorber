88c239c633ac13cb8101f649c11a16c66790fdd1
hipop1109-commits <hipop1109@gmail.com>
2025-01-15 09:36:06 +0900

    weapon middle
    무기 중간점검

--- Assets/Scripts/Hook/RopeActive.cs
+++ Assets/Scripts/Hook/RopeActive.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RopeActive : MonoBehaviour
 {
@@ -9,6 +11,10 @@ public class RopeActive : MonoBehaviour
     public bool isHookActive = false;
     public bool isLineMax;
     public WeaponController weapon;
+    public float elapsedTime = 0;
+    public float ropeCoolTime = 5f;
+    Vector3 mouseDir;
+
 
     private void Start()
     {
@@ -17,6 +23,8 @@ public class RopeActive : MonoBehaviour
         line.SetPosition(0, weapon.Gun.position);
         line.SetPosition(1, hook.position);
         line.useWorldSpace = true;
+
+       
     }
 
     private void Update()
@@ -25,32 +33,42 @@ public class RopeActive : MonoBehaviour
         line.SetPosition(1, hook.position);
     }
 
-    public void RopeAction()
+    public IEnumerator RopeAction()
     {
+
         if (!isHookActive)
         {
             hook.position = weapon.firePoint.position;
+            Vector3 mouseScreenPosition = Mouse.current.position.ReadValue();
+            Vector3 worldMousePosition = UnityEngine.Camera.main.ScreenToWorldPoint(
+                new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, Mathf.Abs(UnityEngine.Camera.main.transform.position.z))
+                );
+            mouseDir = (worldMousePosition - transform.position);
+            mouseDir.z = 0;
             isHookActive = true;
             hook.gameObject.SetActive(true);
         }
 
-        if (isHookActive && !isLineMax)
+        while (!isLineMax)
         {
-            hook.Translate(weapon.mouseWorldPosition.normalized * Time.deltaTime * 50);
-
-            if (Vector2.Distance(transform.position, hook.position) > 50)
+            hook.Translate(mouseDir.normalized * Time.deltaTime * 700);
+            if (Vector2.Distance(transform.position, hook.position) > 10)
             {
                 isLineMax = true;
             }
+            yield return null; // 다음 프레임까지 대기
         }
-        else if (isHookActive && isLineMax)
+
+        while (isLineMax)
         {
             hook.position = Vector2.MoveTowards(hook.position, transform.position, Time.deltaTime * 15);
-            if (Vector2.Distance(transform.position, hook.position) < 0.1f)
+            if (Vector2.Distance(transform.position, hook.position) < 1f)
             {
                 isHookActive = false;
                 isLineMax = false;
+                hook.gameObject.SetActive(false);
             }
+            yield return null; // 다음 프레임까지 대기
         }
     }
 }
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -8,13 +8,13 @@ using UnityEngine.TextCore.Text;
 
 public class WeaponController : MonoBehaviour
 {
-    //���Ƶ��̴� ���� (�ݶ��̴��� ����)
+    //빨아들이는 범위 (콜라이더로 구현)
     public GameObject AbsorbRange;
 
-    //���Ƶ��̰� �ִ� ���¸� ����
+    //빨아들이고 있는 상태를 설정
     private bool isAbsorbing = false;
 
-    //3���� ���Ҹ� ���Ƶ��̰� �ִ� ���� ����
+    //3가지 원소를 빨아들이고 있는 상태 설정
     private bool isRockActive = false;
     private bool isGrassActive = false;
     private bool isWaterActive = false;
@@ -23,24 +23,24 @@ public class WeaponController : MonoBehaviour
     public GameObject WaterEffect;
     public GameObject GrassEffect;
 
-    //������ ���簪
+    //게이지 현재값
     public float RockGauge = 0f;
     public float GrassGauge = 0f;
     public float WaterGauge = 0f;
 
-    //������ �ִ밪
+    //게이지 최대값
     public float MaxGauge = 100f;
-    //�������� ���� �ӵ�
+    //게이지가 차는 속도
     public float FillSpeed = 1f;
 
-    //���� ���� ������
+    //총을 놓을 기준점
     public Transform GunPivot;
     public Transform Gun;
 
-    //���� ���
+    //총의 모드
     public string WeaponMode;
 
-    //�� ���� ����
+    //총 관련 설정
     public GameObject bulletPrefab;
     public GameObject bombPrefab;
     public Transform firePoint;
@@ -50,17 +50,17 @@ public class WeaponController : MonoBehaviour
     private bool canShoot = true;
     public Vector3 mouseWorldPosition;
 
-    //���� ���� ����
-    public GameObject platformPrefab; // ���� ������
-    public float platformSpeed = 5f; // ���� �̵� �ӵ�
-    public float maxPlatformDistance = 1f; // ������ �̵��� �ִ� �Ÿ�
+    //발판 관련 설정
+    public GameObject platformPrefab; // 발판 프리팹
+    public float platformSpeed = 5f; // 발판 이동 속도
+    public float maxPlatformDistance = 1f; // 발판이 이동할 최대 거리
     public int maxPlatforms = 2;
     private List<GameObject> activePlatforms = new List<GameObject>();
 
-    //���� ���� ����
+    //힐링 포션 변수
     public GameObject HealPrefab;
 
-    //���� ����
+    //물총 변수
     public GameObject WaterPrefab;
 
     public RopeActive rope;
@@ -81,9 +81,9 @@ public class WeaponController : MonoBehaviour
         }
     }
 
-        private void Start()
+    private void Start()
     {
-        //���Ƶ��̴� ������ ����
+        //빨아들이는 범위를 꺼둠
         AbsorbRange.SetActive(false);
         RockEffect.SetActive(false);
         WaterEffect.SetActive(false);
@@ -93,42 +93,42 @@ public class WeaponController : MonoBehaviour
     private void Update()
     {
 
-        //���콺�� ���� ��ġ�� Ž���ϴ� ���� ���� (����ī�޶� Ȱ��)
+        //마우스의 현재 위치를 탐색하는 변수 설정 (메인카메라 활용)
         Vector3 mouseScreenPosition = Mouse.current.position.ReadValue();
         mouseWorldPosition = UnityEngine.Camera.main.ScreenToWorldPoint
         (new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, transform.position.z - GunPivot.position.z));
-        
 
-        // �ѱ��� ȸ������ ����
+
+        // 총구의 회전방향 설정
         Vector2 direction = new Vector2(
              mouseWorldPosition.x - GunPivot.position.x,
              mouseWorldPosition.y - GunPivot.position.y
          );
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        //���� �Ӹ� ���� �ٴ� �Ʒ��� �������� ���� �����ϴ� ����
+        //총이 머리 위나 바닥 아래로 지나가면 총이 뒤집하는 조건
         bool shouldFlipGun = angle > 90 || angle < -90;
 
-        //�������� ���� ����
+        //뒤집히는 로직 설정
         if (shouldFlipGun)
         {
-            Gun.rotation = Quaternion.Euler(180, 0, -angle); // ���� ������
+            Gun.rotation = Quaternion.Euler(180, 0, -angle); // 총을 뒤집음
         }
         else
         {
-            Gun.rotation = Quaternion.Euler(0, 0, angle); // ���� ���������� ȸ��
+            Gun.rotation = Quaternion.Euler(0, 0, angle); // 총을 정상적으로 회전
         }
 
     }
 
-    //�������콺 ��������
+    //오른마우스 눌렀을때
     public void AbsorbClick()
     {
         isAbsorbing = true;
         AbsorbRange.SetActive(true);
     }
 
-    //�������콺 ������
+    //오른마우스 뗐을때
     public void AbsorbClickUp()
     {
         isAbsorbing = false;
@@ -141,30 +141,30 @@ public class WeaponController : MonoBehaviour
 
     public void OnAbsorbEffectTriggerStay(Collider2D other)
     {
-        Debug.Log($"OnTriggerStay2D ȣ���: {other.name}");
+        Debug.Log($"OnTriggerStay2D 호출됨: {other.name}");
 
         if (!isAbsorbing)
         {
-            Debug.Log("isAbsorbing�� false���� ���ϵ�");
+            Debug.Log("isAbsorbing이 false여서 리턴됨");
             return;
         }
-        // �±׿� ���� ȿ�� Ȱ��ȭ
+        // 태그에 따라 효과 활성화
         switch (other.tag)
         {
             case "Rock":
-                Debug.Log("�ν�");
+                Debug.Log("인식");
                 ActivateEffect(RockEffect);
                 isRockActive = true;
                 FillGauge();
                 break;
             case "Grass":
-                Debug.Log("�ν�");
+                Debug.Log("인식");
                 ActivateEffect(GrassEffect);
                 isGrassActive = true;
                 FillGauge();
                 break;
             case "Water":
-                Debug.Log("�ν�");
+                Debug.Log("인식");
                 ActivateEffect(WaterEffect);
                 isWaterActive = true;
                 FillGauge();
@@ -174,19 +174,19 @@ public class WeaponController : MonoBehaviour
 
     public void OnAbsorbEffectTriggerExit(Collider2D other)
     {
-       
 
-        // ��� ȿ���� ��Ȱ��ȭ
+
+        // 모든 효과를 비활성화
         DeactivateAllEffects();
     }
 
     private void ActivateEffect(GameObject effect)
     {
-        
-        // �ٸ� ȿ�� ��Ȱ��ȭ
+
+        // 다른 효과 비활성화
         DeactivateAllEffects();
 
-        // ���ϴ� ȿ���� Ȱ��ȭ
+        // 원하는 효과만 활성화
         effect.SetActive(true);
     }
 
@@ -206,67 +206,66 @@ public class WeaponController : MonoBehaviour
         {
             RockGauge += FillSpeed * Time.deltaTime;
             RockGauge = Mathf.Clamp(RockGauge, 0, MaxGauge);
-            Debug.Log("�� ������: " + RockGauge);
+            Debug.Log("돌 게이지: " + RockGauge);
         }
 
         if (isGrassActive)
         {
             GrassGauge += FillSpeed * Time.deltaTime;
             GrassGauge = Mathf.Clamp(GrassGauge, 0, MaxGauge);
-            Debug.Log("Ǯ ������: " + GrassGauge);
+            Debug.Log("풀 게이지: " + GrassGauge);
         }
 
         if (isWaterActive)
         {
             WaterGauge += FillSpeed * Time.deltaTime;
             WaterGauge = Mathf.Clamp(WaterGauge, 0, MaxGauge);
-            Debug.Log("�� ������: " + WaterGauge);
+            Debug.Log("물 게이지: " + WaterGauge);
         }
     }
 
-    //���� ������ (�޸��콺 ������ �� �ߵ�)
+    //무기 고르는 (왼마우스 눌렀을 때 발동)
     public void WeaponSelect()
     {
         switch (WeaponMode)
         {
-            case "�ĵ�Ÿ��":
-                StartCoroutine(WaterSpray()); //�ĵ�Ÿ��
+            case "파도타기":
+                StartCoroutine(WaterSpray()); //파도타기
                 break;
-            case "���� ����":
-                StartCoroutine(HealPotion()); //ȸ������
+            case "힐링 포션":
+                StartCoroutine(HealPotion()); //회복포션
                 break;
-            case "���� �Ѿ�":
-                StartCoroutine(RockBullet()); //���Ѿ�
+            case "바위 총알":
+                StartCoroutine(RockBullet()); //돌총알
                 break;
-            case "���� ��ź":
-                StartCoroutine(RockBomb()); //����ź
+            case "바위 폭탄":
+                StartCoroutine(RockBomb()); //돌폭탄
                 break;
-            case 5:
-                rope.RopeAction(); //��������
+            case "나무덩쿨":
+                StartCoroutine(rope.RopeAction()); //나무덩쿨
                 break;
-            case "���� ����":
-                Debug.Log("�߻�4");
-                RockPlatform(); //����
+            case "발판 생성":
+                RockPlatform(); //발판
                 break;
         }
     }
 
     private IEnumerator RockBullet()
     {
-        Debug.Log("�߻�");
+        Debug.Log("발사");
         if (canShoot && RockGauge > 0 && WaterGauge > 0)
         {
-            Debug.Log("�߻�2");
+            Debug.Log("발사2");
             WaterGauge -= 1f;
             RockGauge -= 1f;
             GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-            // �Ѿ��� Rigidbody2D�� �ӵ� �߰�
+            // 총알의 Rigidbody2D에 속도 추가
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.gravityScale = 0; // �߷� ���� ����
-                rb.linearVelocity = firePoint.right * bulletSpeed; // �߻� ����� �ӵ� ����
+                rb.gravityScale = 0; // 중력 영향 제거
+                rb.linearVelocity = firePoint.right * bulletSpeed; // 발사 방향과 속도 설정
             }
             StartCoroutine(Cooldown(0.2f));
             yield return new WaitForSeconds(2f);
@@ -276,25 +275,25 @@ public class WeaponController : MonoBehaviour
 
     IEnumerator Cooldown(float CoolDown)
     {
-        canShoot = false; // �߻� �Ұ��� ���·� ��ȯ
-        yield return new WaitForSeconds(CoolDown); // ��ٿ� �ð� ��ٸ�
-        canShoot = true; // �߻� ���� ���·� ��ȯ
+        canShoot = false; // 발사 불가능 상태로 전환
+        yield return new WaitForSeconds(CoolDown); // 쿨다운 시간 기다림
+        canShoot = true; // 발사 가능 상태로 전환
     }
 
     private IEnumerator RockBomb()
     {
-        Debug.Log("�߻�3");
+        Debug.Log("발사3");
         if (canShoot && RockGauge > 0)
         {
-            Debug.Log("�߻�2");
+            Debug.Log("발사2");
             RockGauge -= 5f;
             GameObject Bomb = Instantiate(bombPrefab, firePoint.position, firePoint.rotation);
 
-            // �Ѿ��� Rigidbody2D�� �ӵ� �߰�
+            // 총알의 Rigidbody2D에 속도 추가
             Rigidbody2D rb = Bomb.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.linearVelocity = firePoint.right * bulletSpeed; // �߻� ����� �ӵ� ����
+                rb.linearVelocity = firePoint.right * bulletSpeed; // 발사 방향과 속도 설정
             }
             StartCoroutine(Cooldown(3f));
             yield return new WaitForSeconds(2f);
@@ -302,10 +301,10 @@ public class WeaponController : MonoBehaviour
         }
     }
 
-    
+
     public void RockPlatform()
     {
-        Debug.Log("�߻�3");
+        Debug.Log("발사3");
         if (canShoot && RockGauge > 0 && GrassGauge > 0)
         {
             RockGauge -= 5f;
@@ -313,7 +312,7 @@ public class WeaponController : MonoBehaviour
 
             if (activePlatforms.Count >= maxPlatforms)
             {
-                // Ȱ��ȭ�� ������ �ִ� ������ ��� ù ��° ���� ����
+                // 활성화된 발판이 최대 갯수일 경우 첫 번째 발판 제거
                 Destroy(activePlatforms[0]);
                 activePlatforms.RemoveAt(0);
             }
@@ -334,63 +333,63 @@ public class WeaponController : MonoBehaviour
         float traveledDistance = 0f;
         Vector2 lastPosition = platform.transform.position;
         Vector3 initialScale = platform.transform.localScale;
-        Vector3 targetScale = new Vector3(4f, 1f, 1f); // ���� ���� ũ��
+        Vector3 targetScale = new Vector3(4f, 1f, 1f); // 최종 발판 크기
 
         while (traveledDistance < maxPlatformDistance)
         {
-            // ���� ũ�� ���������� ����
+            // 발판 크기 점진적으로 변경
             platform.transform.localScale = Vector3.Lerp(initialScale, targetScale, traveledDistance / maxPlatformDistance);
 
-            // �̵� �Ÿ� ���
+            // 이동 거리 계산
             traveledDistance += Vector2.Distance(lastPosition, platform.transform.position);
             lastPosition = platform.transform.position;
 
             yield return null;
         }
 
-        // ���� ����
-        rb.linearVelocity = Vector2.zero; // �ӵ� ����
-        rb.GetComponent<Collider2D>().enabled = true; // �浹 ���� Ȱ��ȭ
+        // 발판 고정
+        rb.linearVelocity = Vector2.zero; // 속도 제거
+        rb.GetComponent<Collider2D>().enabled = true; // 충돌 가능 활성화
     }
 
     IEnumerator HealPotion()
     {
         if (canShoot && RockGauge > 0 && GrassGauge > 0)
         {
-            //Ŭ����ٿ� ������ �ذ��ؾ��ҵ�?
+            //클릭쿨다운 문제를 해결해야할듯?
             WaterGauge -= 20f;
             GrassGauge -= 20f;
 
             yield return new WaitForSeconds(2f);
             GameObject Bomb = Instantiate(HealPrefab, firePoint.position, firePoint.rotation);
 
-            // �Ѿ��� Rigidbody2D�� �ӵ� �߰�
+            // 총알의 Rigidbody2D에 속도 추가
             Rigidbody2D rb = Bomb.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.linearVelocity = firePoint.right * 5f; // �߻� ����� �ӵ� ����
+                rb.linearVelocity = firePoint.right * 5f; // 발사 방향과 속도 설정
             }
             StartCoroutine(Cooldown(5f));
         }
     }
-    
-    //�� ���� �ڵ�
+
+    //물 무기 코드
     IEnumerator WaterSpray()
     {
-        Debug.Log("�߻�");
+        Debug.Log("발사");
         if (canShoot && WaterGauge > 0)
         {
-            Debug.Log("�߻�2");
+            Debug.Log("발사2");
             WaterGauge -= 5f;
-            
+
             GameObject WaterGun = Instantiate(WaterPrefab, firePoint.position, firePoint.rotation);
 
-            // �Ѿ��� Rigidbody2D�� �ӵ� �߰�
+            // 총알의 Rigidbody2D에 속도 추가
             Rigidbody2D rb = WaterGun.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.gravityScale = 0; // �߷� ���� ����
-                rb.linearVelocity = firePoint.right * 15f; // �߻� ����� �ӵ� ����
+                rb.gravityScale = 0; // 중력 영향 제거
+                rb.linearVelocity = firePoint.right * 15f; // 발사 방향과 속도 설정
             }
             StartCoroutine(Cooldown(1f));
             yield return new WaitForSeconds(3f);
