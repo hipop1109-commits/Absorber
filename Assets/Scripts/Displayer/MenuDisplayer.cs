de649403e6c9c8f9913a238f34037faad4ca0933
kydyne <dainkk55@gmail.com>
2025-01-22 09:54:07 +0900

    Revert "Merge branch 'dy'"
    This reverts commit 8e31b244c1ee01ff448aaaac91a232322a18ae32, reversing
    changes made to 7d6d4b75818d2caa33ea484fb85b19b05a3ea753.

--- Assets/Scripts/Displayer/MenuDisplayer.cs
+++ Assets/Scripts/Displayer/MenuDisplayer.cs
@@ -9,13 +9,11 @@ public class MenuDisplayer : MonoBehaviour
     [SerializeField] private GameObject menuPanel;
 
     // 탭
-    [SerializeField] private GameObject saveTab;
-    [SerializeField] private GameObject resolutionTab;
-    [SerializeField] private GameObject soundTab;
+    [SerializeField]private GameObject resolutionTab;
+    [SerializeField]private GameObject soundTab;
 
-
-    // 세이브 슬롯 상태 텍스트
-    [SerializeField] private TextMeshProUGUI[] slotTexts;
+    // 볼륨 슬라이더
+    [SerializeField]private Slider volumeSlider;
 
     //  해상도 드롭다운
     [SerializeField] private TMP_Dropdown resolutionDropdown;
@@ -23,15 +21,14 @@ public class MenuDisplayer : MonoBehaviour
     // 해상도 목록
     private Resolution[] resolutions;
 
-    // OnOff 버튼 텍스트
+    // 버튼 텍스트
     [SerializeField] private TextMeshProUGUI ButtonText;
-
-    // 볼륨 슬라이더
-    [SerializeField] private Slider volumeSlider;
 
     void Start()
     {
-        UpdateSaveSlots();
+        // 볼륨 슬라이더 초기화
+        volumeSlider.value = AudioListener.volume;
+        volumeSlider.onValueChanged.AddListener(SetVolume);
 
         // 해상도 설정 초기화
         resolutions = Screen.resolutions;
@@ -43,19 +40,10 @@ public class MenuDisplayer : MonoBehaviour
         {
             resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(res.width + " x " + res.height));
         }
-        // 드롭다운 초기 선택값 설정
-        resolutionDropdown.value = 0; // 첫번째 옵션 선택
-        resolutionDropdown.RefreshShownValue(); // UI 갱신
-
-        // 드롭다운 변경 이벤트 리스너 추가
         resolutionDropdown.onValueChanged.AddListener(SetResolution);
 
         // 버튼 텍스트 초기화
         UpdateButtonText();
-
-        // 볼륨 슬라이더 초기화
-        volumeSlider.value = AudioListener.volume;
-        volumeSlider.onValueChanged.AddListener(SetVolume);
     }
 
     private void Update()
@@ -78,38 +66,10 @@ public class MenuDisplayer : MonoBehaviour
         menuPanel.SetActive(false);
     }
     
-    // 슬롯 상태 UI 갱신
-    public void UpdateSaveSlots()
+    // 전체 볼륨 조절
+    public void SetVolume(float volume)
     {
-        for(int i = 0; i < slotTexts.Length; i++)
-        {
-            int slot = i + 1;
-            if (GameManager.IsSlotEmpty(slot))
-            {
-                slotTexts[i].text = "Slot" + slot + " : Empty";
-            }
-            else
-            {
-                var data = GameManager.LoadGame(slot);
-                slotTexts[i].text = "Slot " + slot + ": " + data.playerLife;
-            }
-        }
-    }
-    // 슬롯 클릭
-    public void OnslotClicked(int slot)
-    {
-        if (GameManager.IsSlotEmpty(slot))
-        {
-            GameManager.SaveGame(slot); // 새 데이터를 저장
-            Debug.Log("Game Saved in Slot " + slot);
-        }
-        else
-        {
-            var data = GameManager.LoadGame(slot); // 데이터 기반으로 게임상태를 업데이트 
-            Debug.Log("Game Loaded from Slot " + slot + ": Level " + data.playerPosition);
-        }
-
-        UpdateSaveSlots();
+        AudioListener.volume = volume; 
     }
 
     // 해상도 설정
@@ -138,32 +98,16 @@ public class MenuDisplayer : MonoBehaviour
             ButtonText.text = "Off";
         }
     }
-
-    // 전체 볼륨 조절
-    public void SetVolume(float volume)
-    {
-        AudioListener.volume = volume;
-    }
-    // 세이브 메뉴 열기
-    public void OpenSaveMenu()
-    {
-        saveTab.SetActive(true);
-        resolutionTab.SetActive(false);
-        soundTab.SetActive(false);
-    }
-    // 해상도 메뉴 열기
-    public void OpenResolutionTab()
-    {
-        saveTab.SetActive(false);
-        resolutionTab.SetActive(true);
-        soundTab.SetActive(false);
-    }
-    // 사운드 메뉴 열기
+    // 사운드 탭 열기
     public void OpenSoundTab()
     {
-        saveTab.SetActive(false);
-        resolutionTab.SetActive(false);
         soundTab.SetActive(true);
+        resolutionTab.SetActive(false);
     }
-    
+    // 해상도 탭 열기
+    public void OpenResolutionTab()
+    {
+        soundTab.SetActive(false );
+        resolutionTab.SetActive(true);
+    }
 }
